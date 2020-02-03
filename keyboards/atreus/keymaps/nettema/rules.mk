8967ad0dde23e6413e3cccf4eb0bb9efc5396db4
nettema <nettema@gmail.com>
2020-02-03 11:32:50 +0300

    double tap GUI to change layout layer to use with soundpad (f13-f24 on right half)

--- keyboards/atreus/keymaps/nettema/rules.mk
+++ keyboards/atreus/keymaps/nettema/rules.mk
@@ -64,7 +64,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
-
+TAP_DANCE_ENABLE = yes		# Enable support for tap dance (+1000)
 
 # Bluetooth settings
 # F_CPU = 8000000
